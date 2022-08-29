b8029e7bac885ed9223e0a014b2b05aa88e4c072
AvetSargsyan <awet.sargsyan@mail.ru>
2022-08-29 12:32:54 +0400

    Adding an update to scripts

--- test/specs/bringItOn.js
+++ test/specs/bringItOn.js
@@ -1,21 +1,26 @@
 "use strict";
 const LoginPage = require("../pageobjects/login.page");
+const assert = require("assert");
 
 describe("'Bring It On' task", () => {
-  beforeEach(async () => {
-    await LoginPage.open();
+  afterEach(async () => {
+    await browser.pause(1000);
   });
   it("Should add values to New Paste", async () => {
+    await LoginPage.open();
+    await browser.fullscreenWindow();
     await LoginPage.newPaste.addValue(
       "git config --global user.name 'New Sheriff in Town'\ngit reset $ (git commit-tree HEAD ^ {tree} -m 'Legacy code')\ngit push origin master --force"
     );
   });
-  it("Should put Syntax Highlighting: 'Bash'", async () => {
+  it("Should select Syntax Highlighting: 'Bash'", async () => {
     await LoginPage.syntaxHighlight.click();
+    await browser.pause(1000);
     await LoginPage.bash.click();
   });
-  it("Should put Paste Expiration: '10 Minutes'", async () => {
+  it("Should select Paste Expiration: '10 Minutes'", async () => {
     await LoginPage.pasteExp.click();
+    await browser.pause(1000);
     await LoginPage.tenMinutes.click();
   });
   it("Should write in line Paste Name / Title: 'how to gain dominance among developers'", async () => {
@@ -25,13 +30,24 @@ describe("'Bring It On' task", () => {
   });
   it("Should check if browser page title matches Paste Name / Title", async () => {
     try {
-      expect(browser).toHaveAttribute(LoginPage.pasteName);
+      await expect(browser).toHaveTitle("Paste Name / Title");
     } catch {
-      console.log("Browser page title doesn't match Paste Name / Title");
+      console.log("Page title doesn't matches Paste Name / Title");
     }
   });
   it("Should check if syntax is suspended for bash", async () => {
-    LoginPage.bash.isSelected();
+    LoginPage.bash.isSelected(); // I’m not sure I understood the condition of the task correctly
   });
-  //it("Check that the code matches the one entered in paragraph 2", async()=>{})
+  it("Check that the code matches the one entered in paragraph 2", async () => {
+    await assert(
+      LoginPage.newPaste.getText(),
+      "git config --global user.name 'New Sheriff in Town'\ngit reset $ (git commit-tree HEAD ^ {tree} -m 'Legacy code')\ngit push origin master --force"
+    );
+    await assert(LoginPage.bash.getAttribute("aria-selected"), "true");
+    await assert(LoginPage.tenMinutes.getAttribute("aria-selected"), "true");
+    await assert(
+      LoginPage.pasteName.getText(),
+      "how to gain dominance among developers"
+    );
+  });
 });
--- test/specs/iCanWin.js
+++ test/specs/iCanWin.js
@@ -1,17 +1,22 @@
+"use strict";
 const LoginPage = require("../pageobjects/login.page");
 
 describe("'I Can Win' task", () => {
-  beforeEach(async () => {
-    await LoginPage.open();
+  afterEach(async () => {
+    await browser.pause(1000);
   });
-  it("Shoult add values to New Paste: 'Hello from WebDriver'", async () => {
+  it("Shoult open page nad add value to New Paste: 'Hello from WebDriver'", async () => {
+    await LoginPage.open();
+    await browser.fullscreenWindow();
     await LoginPage.newPaste.addValue("Hello from WebDriver");
   });
-  it("Should put Paste Expiration: '10 Minutes'", async () => {
+  it("Should select Paste Expiration: '10 Minutes'", async () => {
     await LoginPage.pasteExp.click();
+    await browser.pause(1000);
     await LoginPage.tenMinutes.click();
   });
   it("Should put Paste Name / Title: 'helloweb'", async () => {
     await LoginPage.pasteName.addValue("helloweb");
+    await browser.pause(3000);
   });
 });
