d6feb2e50386c18304ed9ba596d50d7192b008f9
AvetSargsyan <91004418+AvetSargsyan@users.noreply.github.com>
2022-09-17 11:29:38 +0400

    Update iCanWin.js

--- test/specs/iCanWin.js
+++ test/specs/iCanWin.js
@@ -2,7 +2,7 @@
 const MainPage = require("../pageobjects/main.page");
 
 describe("'I Can Win' task", () => {
-  it("Shoult open page, add some values, select some options", async () => {
+  it("Should open page, add some values, select some options", async () => {
     await MainPage.open();
     await MainPage.check("helloweb", "Hello from WebDriver");
   });
